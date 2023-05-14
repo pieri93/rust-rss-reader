d7d4567303fd1ac1d9bc318ce89decbea68a2d31
Pierina Ponce <pierinaponce1993@gmail.com>
2023-05-14 19:56:37 -0300

    Adds the rss reader

--- rust-rss-reader/src/main.rs
+++ rust-rss-reader/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use rss::Channel;
+use std::error::Error;
+use std::io;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut url = String::new();
+
+    // Ask user's input
+    println!("Enter RSS feed URL: ");
+    io::stdin().read_line(&mut url)?;
+
+    let url = url.trim();
+
+    let resp = reqwest::blocking::get(url)?;
+    let body = resp.bytes()?;
+    let channel = Channel::read_from(&body[..])?;
+
+    println!("Title: {}", channel.title());
+    println!("Link: {}", channel.link());
+    println!("Description: {}", channel.description());
+
+    for item in channel.items() {
+        println!("Title: {}", item.title().unwrap_or_default());
+        println!("Link: {}", item.link().unwrap_or_default());
+        println!("Description: {}", item.description().unwrap_or_default());
+        println!("Published: {}", item.pub_date().unwrap_or_default());
+        println!();
+    }
+
+    println!("{}", url);
+    Ok(())
 }
